fc7a4891dccc945f379a7d9ed8e7f13cf54a8e76
Zoaib <thisiszoaib@gmail.com>
2022-01-11 12:54:25 +0500

    Final tutorial completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-product-config';
+  
+  colors: string[] = [
+    '#b3478c', '#1e62c0', '#ffa764', '#3de68b', '#a11f2a', '#ffbf00'
+  ];
+  
+  selectedColor = this.colors[0];
+
 }
